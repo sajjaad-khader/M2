792d6b7c5d9722ca6b76c5cd008953803c22f319
sajjaad-khader <54915943+sajjaad-khader@users.noreply.github.com>
2020-01-27 01:29:18 -0500

    Add files via upload

---
+++ M2/src/Person3.java
@@ -0,0 +1,15 @@
+/**
+ *  TODO: Change the author to your full name
+ *
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person3 {
+
+  private String name;
+
+  public Person1(String name) {
+    this.name = name;
+  }
+
+}
